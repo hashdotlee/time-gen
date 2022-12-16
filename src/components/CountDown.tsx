e6420eb2dea77ee0ad6a9f1f47958dd3c7df74ba
Le Viet Hoang <hashdotlee@gmail.com>
2022-12-16 19:04:01 +0700

    update date format

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,24 +2,29 @@ import { useEffect, useState } from "react";
 
 export default function CountDown({ cn }: { cn?: string }) {
   const [hour, setHour] = useState(0);
+  const [day, setDay] = useState(0);
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
   useEffect(() => {
     const t = setTimeout(() => {
       const timeLeft =
         (new Date("12/20/2023").getTime() - new Date().getTime()) / 1000;
-      const h = Math.floor(timeLeft / 3600);
-      const m = Math.floor((timeLeft - h * 3600) / 60);
-      const s = Math.floor(timeLeft - h * 3600 - m * 60);
+      const d = Math.floor(timeLeft / (3600 * 24));
+      const h = Math.floor((timeLeft - d * 3600 * 24) / 3600);
+      const m = Math.floor((timeLeft - h * 3600 - d * 3600 * 24) / 60);
+      const s = Math.floor(timeLeft - h * 3600 - m * 60 - d * 3600 * 24);
       setHour(h);
       setMinute(m);
       setSecond(s);
+      setDay(d);
     }, 1000);
     return () => clearTimeout(t);
   });
   return (
     <span className={cn}>
-      {hour < 10 ? "0" + hour : hour} : {minute < 10 ? "0" + minute : minute} : {second < 10 ? "0" + second : second}
+      {day < 10 ? "0" + day : day}D : {hour < 10 ? "0" + hour : hour}H :{" "}
+      {minute < 10 ? "0" + minute : minute}M :{" "}
+      {second < 10 ? "0" + second : second}S
     </span>
   );
 }
