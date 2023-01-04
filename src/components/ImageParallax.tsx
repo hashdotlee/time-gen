af0c01df5570eda3d0a0aa2f3fea7809dcef3d40
Le Viet Hoang <hashdotlee@gmail.com>
2023-01-04 19:18:27 +0700

    fix bugs

--- src/components/ImageParallax.tsx
+++ src/components/ImageParallax.tsx
@@ -9,24 +9,9 @@ interface Props {
 }
 
 export default function ImageParallax({ width, height, src, alt }: Props) {
-  const { ref, inView, entry } = useInView({
-    trackVisibility: true,
-    threshold: 0.5,
-  });
-  const [offset, setOffset] = useState(0);
-
-  useEffect(() => {
-    const onScroll = () => setOffset(window.pageYOffset % window.innerHeight);
-    // clean up code
-    window.removeEventListener("scroll", onScroll);
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-  console.log(entry?.intersectionRect);
 
   return (
     <div
-      ref={ref}
       className="overflow-hidden relative"
       style={{ width, height }}
     >
@@ -34,9 +19,6 @@ export default function ImageParallax({ width, height, src, alt }: Props) {
         src={src}
         alt={alt}
         className="transition-transform border shadow-sm ease-linear absolute z-[-1] top-[-50%]"
-        style={{
-          transform: `translateY(${offset / 10}px)`,
-        }}
       />
     </div>
   );
